317b72ef1c5f618d9d5efbb250288c1387eed698
Radforga <radmir2301@gmail.com>
2024-06-06 21:59:21 +0300

    Добавьте файлы проекта.

---
+++ Pacient.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Tracing;
+
+namespace MIS
+{
+    public class DiagnosisType
+    {
+        public int ID { get; set; }
+        [DisplayName("Диагноз")]
+        public String name { get; set; }
+        [DisplayName("Описание")]
+        [DataType(DataType.MultilineText)]
+        public String description { get; set; }
+    }
+    public class Diagnosis
+    {
+        public int ID { get; set; }
+        [DisplayName("Диагноз")]
+        public DiagnosisType type { get; set; }
+        [DisplayName("Комментарий")]
+        [DataType(DataType.MultilineText)]
+        public String comment { get; set; }
+    }
+    public class Pacient
+    {
+
+    }
+}
